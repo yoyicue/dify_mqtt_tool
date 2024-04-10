160e38a029ac0be1dfdec43b37cc8e8152fcd06c
wensheng <yoyicue@gmail.com>
2024-04-10 10:46:13 +0800

    Modify the part with confused logic.

--- tools/multiple_publish.py
+++ tools/multiple_publish.py
@@ -6,9 +6,7 @@ from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.tool.builtin_tool import BuiltinTool
 
 class MultiplePublishTool(BuiltinTool):
-    def _invoke(self, user_id: str, tool_parameters: dict[str, Any]) -> ToolInvokeMessage | list[ToolInvokeMessage]:
-        return super()._invoke(user_id, tool_parameters)
-    
+    def _invoke(self, user_id: str, tool_parameters: dict[str, Any]) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
         """
         Paho Matt Publish Invoke
         import paho.mqtt.publish as publish
@@ -29,7 +27,9 @@ class MultiplePublishTool(BuiltinTool):
         try:
             if username:
                 publish.multiple(msgs, hostname=host, port=port, auth={'username': username, 'password': password})
+                return self.create_text_message(self.summary(user_id=user_id, content=json.dumps(msgs, ensure_ascii=False)))
             else:
                 publish.multiple(msgs, hostname=host, port=port)
+                return self.create_text_message(self.summary(user_id=user_id, content=json.dumps(msgs, ensure_ascii=False)))
         except Exception as e:
             return self.create_text_message(f"Failed to send the message, error: {e}")
--- tools/single_publish.py
+++ tools/single_publish.py
@@ -1,4 +1,3 @@
-
 from typing import Any
 import paho.mqtt.publish as publish
 
@@ -6,9 +5,7 @@ from core.tools.entities.tool_entities import ToolInvokeMessage
 from core.tools.tool.builtin_tool import BuiltinTool
 
 class SinglePublishTool(BuiltinTool):
-    def _invoke(self, user_id: str, tool_parameters: dict[str, Any]) -> ToolInvokeMessage | list[ToolInvokeMessage]:
-        return super()._invoke(user_id, tool_parameters)
-    
+    def _invoke(self, user_id: str, tool_parameters: dict[str, Any]) -> Union[ToolInvokeMessage, list[ToolInvokeMessage]]:
         """
         Paho Matt Publish Invoke
         publish.single("paho/test/topic", "payload", hostname="mqtt.eclipseprojects.io")
@@ -30,7 +27,9 @@ class SinglePublishTool(BuiltinTool):
         try:
             if username:
                 publish.single(topic, message, hostname=host, port=port, auth={'username': username, 'password': password})
+                return self.create_text_message(self.summary(user_id=user_id, content=json.dumps(message, ensure_ascii=False)))
             else:
                 publish.single(topic, message, hostname=host, port=port)
+                return self.create_text_message(self.summary(user_id=user_id, content=json.dumps(message, ensure_ascii=False)))
         except Exception as e:
             return self.create_text_message(f"Failed to send the message, error: {e}")
